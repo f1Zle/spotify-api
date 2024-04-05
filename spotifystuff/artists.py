d219636d8ebcd83206fbbaf02d099ccb1343b2e4
f1Zle <cubby497@gmail.com>
2024-04-04 23:22:22 -0600

    deleted:    Untitled-1.js 	deleted:    Untitled-2.js 	deleted:    Untitled-3.js 	deleted:    const playlistId = '0Wznxl2TStiJyGXVjaob.js 	new file:   notes.txt 	new file:   patchnotes.txt 	renamed:    artists.py -> spotifystuff/artists.py 	renamed:    testtest.py -> spotifystuff/genre.py

--- spotifystuff/artists.py
+++ spotifystuff/artists.py
@@ -1,6 +1,7 @@
 import spotipy
 import requests
 import json
+import time
 from spotipy.oauth2 import SpotifyOAuth
 client_id = '14ecafe25d67441690c29c28c507ebdd'
 client_secret = '1432bcdbf5ad47f99c23d6bd6582c3c4'
@@ -11,40 +12,28 @@ redirect_uri = 'http://localhost:8888/callback'
 scope = 'playlist-modify-private,user-library-read'
 
 session = requests.Session()
-session.headers["User-Agent"] = "Your User Agent"  # Specify your user agent
-session.headers["Accept-Language"] = "en-US"  # Specify your preferred language
-session.headers["Accept-Encoding"] = "gzip, deflate, br"  # Specify your preferred encoding
-session.headers["Accept"] = "application/json"  # Specify your preferred response format
-session.timeout = 10900000  # Set the read timeout to 10 seconds
+session.headers["User-Agent"] = "Your User Agent"  
+session.headers["Accept-Language"] = "en-US"  
+session.headers["Accept-Encoding"] = "gzip, deflate, br"  
+session.headers["Accept"] = "application/json"  
+session.timeout = 10  
 
-# Initialize Spotify client with custom session
+
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,
                                                client_secret=client_secret,
                                                redirect_uri=redirect_uri,
                                                scope=scope,
-                                               open_browser=False),  # Disable opening browser for authentication
-                      requests_session=session)  # Pass the custom session
+                                               open_browser=False),
+                      requests_session=session)  
 user_id = sp.current_user()['id']
-playlist = sp.user_playlist_create(user_id, '$B', public=False)
-# playlist_id = playlist_response['id']
+playlist = sp.user_playlist_create(user_id, 'nothing,nowhere', public=False)
 print(f'Playlist "Hello World" created with ID: {playlist["id"]}')
 
-# track_ID =[]
-
-# def get_all_liked_songs():
-#     results = sp.current_user_saved_tracks()
-#     liked_songs = results['items']
-
-#     while results['next']:
-#         results = sp.next(results)
-#         liked_songs.extend(results['items'])
-
-#     return liked_songs
 
 def search_saved_tracks_by_artist(artists):
     with open('info.txt', 'w+', encoding='utf-8') as file:
         offset = 0
-        limit = 50  # Maximum limit per request
+        limit = 50 
         track_ID = []
 
         while True:
@@ -56,18 +45,18 @@ def search_saved_tracks_by_artist(artists):
                 if any(artist in artist_names for artist in artists):
                     print(f'Found track {track["name"]} by {", ".join(artist_names)}')
                     track_ID.append(track['id'])
-
-            # Check if there are more tracks to fetch
+            time.sleep(1)
+            
             if len(results['items']) < limit:
                 break
             else:
                 offset += limit
 
-        batch_size = 100  # Set the batch size
+        batch_size = 100 
         for i in range(0, len(track_ID), batch_size):
             sp.playlist_add_items(playlist["id"], track_ID[i:i+batch_size])
 
-search_saved_tracks_by_artist(['$uicideboy$'])  # Replace 'artist1', 'artist2', 'artist3' with the desired artist names
+search_saved_tracks_by_artist(['nothing,nowhere']).lower()
 
 
 
--- spotifystuff/genre.py
+++ spotifystuff/genre.py
@@ -1,6 +1,6 @@
 import spotipy
 import requests
-import json
+import time
 from spotipy.oauth2 import SpotifyOAuth
 client_id = '4fe94719ff284848b501b361e38512d5'
 client_secret = 'a697afc8c97f441abdf1c108f6b9c229'
@@ -11,40 +11,30 @@ redirect_uri = 'http://localhost:3000'
 scope = 'playlist-modify-private,user-library-read'
 
 session = requests.Session()
-session.headers["User-Agent"] = "Your User Agent"  # Specify your user agent
-session.headers["Accept-Language"] = "en-US"  # Specify your preferred language
-session.headers["Accept-Encoding"] = "gzip, deflate, br"  # Specify your preferred encoding
-session.headers["Accept"] = "application/json"  # Specify your preferred response format
-session.timeout = 10  # Set the read timeout to 10 seconds
+session.headers["User-Agent"] = "Your User Agent" 
+session.headers["Accept-Language"] = "en-US"  
+session.headers["Accept-Encoding"] = "gzip, deflate, br"  
+session.headers["Accept"] = "application/json" 
+session.timeout = 10  
 
-# Initialize Spotify client with custom session
+
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,
                                                client_secret=client_secret,
                                                redirect_uri=redirect_uri,
                                                scope=scope,
-                                               open_browser=False),  # Disable opening browser for authentication
-                      requests_session=session)  # Pass the custom session
+                                               open_browser=False),  
+                      requests_session=session)  
 user_id = sp.current_user()['id']
-playlist = sp.user_playlist_create(user_id, '$B', public=False)
-# playlist_id = playlist_response['id']
+playlist = sp.user_playlist_create(user_id, ':(', public=False)
+
 print(f'Playlist "Hello World" created with ID: {playlist["id"]}')
 
-# track_ID =[]
 
-# def get_all_liked_songs():
-#     results = sp.current_user_saved_tracks()
-#     liked_songs = results['items']
-
-#     while results['next']:
-#         results = sp.next(results)
-#         liked_songs.extend(results['items'])
-
-#     return liked_songs
 
 def search_saved_tracks_by_genre(genres):
     with open('info.txt', 'w+', encoding='utf-8') as file:
         offset = 0
-        limit = 50  # Maximum limit per request
+        limit = 50  
         track_ID = []
 
         while True:
@@ -54,22 +44,25 @@ def search_saved_tracks_by_genre(genres):
                 artist_id = track['artists'][0]['id']
                 artist = sp.artist(artist_id)
                 track_genres = artist['genres']
-
+                time.sleep(1)
                 if any(genre in track_genres for genre in genres):
                     print(f'Found track {track["name"]} by {track["artists"][0]["name"]} in genres {genres}')
                     track_ID.append(track['id'])
-
-            # Check if there are more tracks to fetch
+            time.sleep(3)
+           
             if len(results['items']) < limit:
                 break
             else:
                 offset += limit
 
-        # Add the selected tracks to the playlist
-        sp.playlist_add_items(playlist["id"], track_ID)
+        
+        batch_size = 50 
+        for i in range(0, len(track_ID), batch_size):
+            sp.playlist_add_items(playlist["id"], track_ID[i:i+batch_size])
+            time.sleep(1)
 
  
-search_saved_tracks_by_genre(['rock'])
+search_saved_tracks_by_genre(['emo rap'])
 
 
 
